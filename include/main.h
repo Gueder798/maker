dc82c0ec26197e56a4db28468d450ec33613cff9
Gueder798 <zhang413917@outlook.com>
2025-10-11 21:30:44 +0800

    fixed soem problems in Linux

--- include/main.h
+++ include/main.h
@@ -2,7 +2,11 @@
 
 #include <stdio.h>
 #include "yaml-cpp/yaml.h"
+#ifdef WIN32
 #include <string.h>
+#else 
+#include <strings.h>
+#endif
 #include <vector>
 #include "get.h"
 #include <stdlib.h>
@@ -14,4 +18,5 @@
 #elif __LINUX__
 #define popen popen
 #define pclose pclose
+#define stricmp strcasecmp
 #endif
